4d47a784a6bc923715ff8493b572552f8305fc63
ElielMato <elielmato360@gmail.com>
2024-12-11 11:51:12 -0300

    clone-tesla-landing

---
+++ src/data/menuItems.ts
@@ -0,0 +1,34 @@
+import { MenuItem } from "../types/types"
+
+const Items: MenuItem[] = [
+    {
+        title: "Model S",
+        url: "/models"
+    },
+    {
+        title: "Model 3",
+        url: "/model3"
+    },
+    {
+        title: "Model X",
+        url: "/modelx"
+    },
+    {
+        title: "Model Y",
+        url: "/modely"
+    },
+    {
+        title: "Solar Roof",
+        url: "/solarroof"
+    },
+    {
+        title: "Solar Panels",
+        url: "/solarpanels"
+    },
+    {
+        title: "Powerwall",
+        url: "/powerwall"
+    },
+]
+
+export default Items
